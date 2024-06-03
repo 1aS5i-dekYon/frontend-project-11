d80f8e9f5a25d363ec42d4728075362d585df522
da.lasitsa <neolakalasua641@gmail.com>
2024-06-03 22:59:12 +0200

    UPDATE: src/, index.html, tools

--- src/init.js
+++ src/init.js
@@ -3,6 +3,38 @@ import i18next from 'i18next';
 import axios from 'axios';
 import renderRssForm from './view.js';
 
+let lastPostId = 0;
+const makePostId = () => {
+  lastPostId += 1;
+  return lastPostId;
+};
+let lastFeedId = 0;
+const makeFeedId = () => {
+  lastFeedId += 1;
+  return lastFeedId;
+};
+
+const makeFeed = (feedId, doc, url) => {
+  const titleFeed = doc.querySelector('title');
+  const descriptionFeed = doc.querySelector('description');
+  return {
+    feedId, titleFeed, descriptionFeed, url,
+  };
+};
+const makePosts = (feedId, doc) => {
+  const items = doc.querySelectorAll('item');
+  const posts = items.forEach((el) => {
+    makePostId();
+    const postId = lastPostId;
+    const titlePost = el.querySelector('title');
+    const descriptionPost = el.querySelector('description');
+    return {
+      postId, feedId, titlePost, descriptionPost,
+    };
+  });
+  return posts;
+};
+
 const getHttpResponseData = (url) => axios.get(`https://allorigins.hexlet.app/get?disableCache=true&url=${url}`)
   .then((response) => response.data)
   .catch(() => new Error('networkError'));
@@ -34,6 +66,9 @@ export default () => {
     resources: {
       ru: {
         translation: {
+          titleFeeds: 'Фиды',
+          titlePosts: 'Посты',
+          preview: 'Просмотр',
           validUrl: 'RSS успешно загружен',
           errors: {
             invalidUrl: 'Ссылка должна быть валидным URL',
@@ -49,19 +84,13 @@ export default () => {
   });
 
   const state = {
+    process: '',
     form: {
-      state: 'filling',
       fields: { url: '' },
       error: '',
     },
-    modal: {
-      title: '',
-      description: '',
-      link: '',
-    },
     feeds: [],
     posts: [],
-    readPostIds: new Set(),
   };
 
   const watchedState = renderRssForm(state, i18nextInstance);
@@ -72,21 +101,31 @@ export default () => {
     const formData = new FormData(e.target);
     const url = formData.get('url');
 
-    const loadedFeeds = Object.values(state.feeds).map((item) => item.requestUrl);
+    const loadedFeeds = Object.values(state.feeds).map((item) => item.url);
 
     const formSchema = yup.string()
       .url()
       .required()
       .notOneOf(loadedFeeds);
-    // const url = 'http://feeds.feedburner.com/Archdaily';
-
+    // 'http://feeds.feedburner.com/Archdaily'
+    // https://www.aljazeera.com/xml/rss/all.xml
+    watchedState.process = 'filling';
     formSchema
       .validate(url)
       .then(() => getHttpResponseData(url))
-      .then((data) => {
-        // принимаем doc и конфигурим посты и фиды для стейта
-        getParsedDataRss(data);
-        // каждому посту и фиду свой ID в том числе и для следования канонам нормализации данных
+      .then((data) => getParsedDataRss(data))
+      .then((docXML) => {
+        // отправляем url  в feed и не паримся, а извлечет посты из него уже Fn с setTimeout!!!
+        // все что ниже удалаяем отсюда и пихаем в эту Fn
+        makeFeedId();
+        const newFeed = makeFeed(lastFeedId, docXML, url);
+        const newPosts = makePosts(lastFeedId, docXML);
+
+        watchedState.feeds.push(newFeed);
+        watchedState.posts.push(...newPosts);
+        // после setTimeout обновляются все ID постов и фидов ...
+        // ... (lastFeedId и lastPostId присваиваются нулю)
+        // modal создается в начале body и обновляется по клику на новый пост
       })
       .catch((error) => {
         watchedState.form.error = error.message ?? 'default';
--- src/view.js
+++ src/view.js
@@ -1,5 +1,61 @@
 import onChange from 'on-change';
 
+const createEl = (tagName, style = '', text = '') => {
+  const element = document.createElement(tagName);
+  if (style) element.classList.add(...style);
+  if (text) element.textContent = text;
+  return element;
+};
+
+const makeBoxFor = (els = '', text = '') => {
+  const container = document.querySelector('.feeds');
+
+  const cardBorder = createEl('div', ['card-border-0']);
+  const cardBody = createEl('div', ['card-body']);
+  const title = createEl('h2', ['card-title', 'h4'], text);
+  const list = createEl('ul', ['list-group', 'border-0', 'rounded-0']);
+
+  list.append(...els);
+  cardBody.append(title);
+  cardBorder.append(cardBody, list);
+  container.replaceChildren(cardBorder);
+};
+
+const makePostsBox = (value, text) => {
+  const postsList = value.map((post) => {
+    const el = createEl('li', [
+      'list-group-item', 'd-flex', 'justify-content-between',
+      'align-items-start', 'border-0', 'border-end-0']);
+
+    const link = createEl('a', ['fw-bold']);
+    link.setAttribute('data-id', post.postId);
+    link.setAttribute('href', post.link);
+    link.setAttribute('target', '_blank');
+    link.setAttribute('rel', 'noopener noreferrer');
+    link.textContent = post.titlePost;
+
+    const button = createEl('button', ['btn', 'btn-outline-primary', 'btn-sm']);
+    button.setAttribute('type', 'button');
+    button.setAttribute('data-id', post.postId);
+    button.setAttribute('data-bs-toggle', 'modal');
+    button.setAttribute('data-bs-target', '#modal');
+
+    el.append(link, button);
+    return el;
+  });
+  makeBoxFor(postsList, text);
+};
+
+const makeFeedsBox = (feeds, text) => {
+  const feedsList = feeds.map(({ descriptionFeed, titleFeed }) => {
+    const elList = createEl('li', ['list-group-item', 'border-0', 'border-end-0']);
+    const titleEl = createEl('li', '', titleFeed);
+    const descriptionEl = createEl('li', ['m-0 small', 'text-black-50'], descriptionFeed);
+    return elList.append(titleEl, descriptionEl);
+  });
+  makeBoxFor(feedsList, text);
+};
+
 const makeSuccessParagraph = (input, p, text) => {
   input.classList.remove('is-invalid');
   input.classList.add('is-valid');
@@ -11,20 +67,22 @@ const makeDangerParagraph = (input, p, text) => {
   p.innerHTML = text;
 };
 
-export default (state, i18nextInstance) => onChange(state, (path) => {
-  // в зависимости от результата валидации выводит рамку инпута
+export default (state, i18nextInstance) => onChange(state, (path, value) => {
   const urlInput = document.querySelector('#url-input');
   const p = document.querySelector('.feedback');
-
   switch (path) {
+    case 'process':
+      break;
     case 'form.error':
       makeDangerParagraph(urlInput, p, i18nextInstance.t(state[path]));
       break;
-
     case 'feeds':
+      makeFeedsBox(value, i18nextInstance.t('titleFeeds'));
       makeSuccessParagraph(urlInput, p, i18nextInstance.t(state.validUrl));
       break;
-
+    case 'posts':
+      makePostsBox(value, i18nextInstance.t('titlePosts'));
+      break;
     default:
       throw new Error('boom');
   }
